5951028f54f8a2224b1560442c4a32e64e0d6ce2
jillraz99 <38738176+jillraz99@users.noreply.github.com>
2023-09-13 14:32:27 -0600

    calculator edits

--- week1/calculator.js
+++ week1/calculator.js
@@ -1,38 +1,24 @@
-let calculator = {
-    describe("calculator", function() {
+    
+     
+  let calculator = {
+    sum() {
+      return this.a + this.b;
+    },
   
-        context("when 2 and 3 entered", function() {
-          beforeEach(function() {
-            sinon.stub(window, "prompt");
-      
-            prompt.onCall(0).returns("2");
-            prompt.onCall(1).returns("3");
-      
-            calculator.read();
-          });
-      
-          afterEach(function() {
-            prompt.restore();
-          });
-          
-          it('the read get two values and saves them as object properties', function () {
-            assert.equal(calculator.a, 2);
-            assert.equal(calculator.b, 3);
-          });
-      
-          it("the sum is 5", function() {
-            assert.equal(calculator.sum(), 5);
-          });
-      
-          it("the multiplication product is 6", function() {
-            assert.equal(calculator.mul(), 6);
-          });
-        });
-      
-      });
-    };
+    mul() {
+      return this.a * this.b;
+    },
+
+    sub() {
+        return this.a - this.b;
+    },
+
+    div() {
+        return this.a / this.b;
+    },
   
-  calculator.read();
-  alert( calculator.sum() );
-  alert( calculator.mul() );
-  
+    read() {
+      this.a = +prompt('enter first number', 0);
+      this.b = +prompt('enter second number', 0);
+    }
+  }
